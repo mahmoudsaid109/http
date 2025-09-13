53efebe5b14889f6da570167143bdb5229d25b14
si3aa <mahmoud22said22@gmail.com>
2025-09-13 14:38:15 +0300

    create project files and get data successfully

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,49 @@
+import {
+  Component,
+  DestroyRef,
+  inject,
+  Injectable,
+  OnInit,
+  signal,
+} from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  isFeatching = signal(false);
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  // constructor(private httpClient: HttpClient) {}
+   ngOnInit() {
+    this.isFeatching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places)
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        complete: () => {
+          this.isFeatching.set(false);
+        }
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
