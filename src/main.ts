f8f634416cbf30d17b672b7b924fe65d012be1a3
si3aa <mahmoud22said22@gmail.com>
2025-09-16 18:33:51 +0300

    print response body and status code using withInterceptors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,35 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { tap } from 'rxjs';
 
-
-bootstrapApplication(AppComponent,{
-    providers: [provideHttpClient()]
+function loggingInerceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  //   const req = request.clone({
+  //     setHeaders: {
+  //       Authorization: 'Bearer token',
+  //     },
+  //   });
+  console.log('OutGoing Request');
+  console.log(request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Incoming Response');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
+}
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(withInterceptors([loggingInerceptor]))],
 }).catch((err) => console.error(err));
