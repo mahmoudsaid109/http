8358e7fcea221a0bae310efff1335885b9712160
si3aa <mahmoud22said22@gmail.com>
2025-09-16 17:27:01 +0300

    create error model to handle errors messages

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -24,18 +24,32 @@ export class PlacesService {
     return this.fetchPlaces(
       'http://localhost:3000/user-places',
       'Something went wrong fetching your favorite places.please try again later.'
-    ).pipe(tap({
-      next: (places) => {
-        this.userPlaces.set(places);
-      }
-    }));
+    ).pipe(
+      tap({
+        next: (places) => {
+          this.userPlaces.set(places);
+        },
+      })
+    );
   }
 
   addPlaceToUserPlaces(place: Place) {
-    this.userPlaces.update((prevPlaces) => [...prevPlaces, place]);
-    return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId:place.id,
-    });
+    const prevPlaces = this.userPlaces();
+    if (!prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set([...prevPlaces, place]);
+    }
+    return this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: place.id,
+      })
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces);
+          return throwError(
+            () => new Error('Something went wrong adding place to user places')
+          );
+        })
+      );
   }
 
   removeUserPlace(place: Place) {}
